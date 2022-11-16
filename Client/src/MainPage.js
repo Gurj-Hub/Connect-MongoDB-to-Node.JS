190007c5d8bbb6ba5798d6e5a72f682fc9af8772
Gurjinder Singh Bhandal <g.singh.bhandal@outlook.com>
2022-11-16 00:24:09 -0500

    Created main instruction page + set up App

---
+++ Client/src/MainPage.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const MainPage = () => {
+  return (
+    <>
+      <h1>This is the TITLE!!</h1>
+      <h3>Should be the SUBHEADER!</h3>
+      <div> Should be the text for instructions.</div>
+    </>
+  );
+};
+
+export default MainPage;
